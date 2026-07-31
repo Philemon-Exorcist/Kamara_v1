fee5377a28b7f212fac329391dd1c0113bbe0d21
philemon <progressphilemon@gmail.com>
2026-07-31 15:02:27 +0100

    api

--- public/app/routes/api-config.ts
+++ public/app/routes/api-config.ts
@@ -4,10 +4,10 @@ export const PRODUCTION_API_URL = "https://kamara.onrender.com";
 /**
  * Determines the correct API base URL based on the current environment.
  */
-export function getBaseUrl() {
-  if (typeof window !== "undefined" && window.location.hostname === "localhost") {
-    return LOCAL_API_URL;
-  }
+// export function getBaseUrl() {
+//   if (typeof window !== "undefined" && window.location.hostname === "localhost") {
+//     return LOCAL_API_URL;
+//   }
 
-  return PRODUCTION_API_URL;
-}
+//   return PRODUCTION_API_URL;
+// }
