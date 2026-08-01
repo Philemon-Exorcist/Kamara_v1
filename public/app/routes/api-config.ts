d914bd3b52341735d716d30610d11b07daea4898
AI Apostle <achasaviourikhamate@gmail.com>
2026-08-01 01:00:31 +0100

    fixed the url problem

--- public/app/routes/api-config.ts
+++ public/app/routes/api-config.ts
@@ -1,14 +1,15 @@
 
 export const PRODUCTION_API_URL = "https://kamara.onrender.com/api/v1";
 export const PRODUCTION_WEBSOCKET_URL = "wss://kamara.onrender.com/ws/api/v1";
-// export const LOCAL_API_URL = "http://localhost:8001/api/v1";
+export const LOCAL_API_URL = "http://localhost:8001/api/v1";
 //export const PRODUCTION_API_URL = "https://kamara.onrender.com";
 
 /**
  * Frontend API base URL.
  */
 export function getBaseUrl() {
-  return PRODUCTION_API_URL;
+ // return PRODUCTION_API_URL;
+  return LOCAL_API_URL;
 }
 
 /**
@@ -16,4 +17,5 @@ export function getBaseUrl() {
  */
 export function getWebSocketBaseUrl() {
   return PRODUCTION_WEBSOCKET_URL;
+  //return LOCAL_WEBSOCKET_URL;
 }
